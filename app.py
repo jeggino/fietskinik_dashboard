04a0c2af049f664698572933263041c171efe646
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-25 10:58:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,19 +23,17 @@ if len(df_filter_data)==0:
 else:
   
   left, right = st.columns(2)
-  with left:
-    time_shift = st.selectbox('Chose a time shift',time_shift_choice)
-    
-  with right:    
-    df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].drop("key",axis=1).sort_values("time_shift").reset_index(drop=True)
-    n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
-    n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
-    n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
+  col1, col2, col3, col4 = st.columns(4)
+  
+  time_shift = col1.st.selectbox('Chose a time shift',time_shift_choice)
+  df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
+  n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
+  n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
+  n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
 
-    col1, col2, col3 = st.columns(3)
-    col1.metric("14-16", f"{n_1} clients")
-    col2.metric("16-18", f"{n_2} clients")
-    col3.metric("18-20", f"{n_3} clients")
+  col2.metric("14-16", f"{n_1} clients")
+  col3.metric("16-18", f"{n_2} clients")
+  col4.metric("18-20", f"{n_3} clients")
     
   st.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
 
