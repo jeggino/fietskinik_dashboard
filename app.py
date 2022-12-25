22950817f83c601d108f925df8e9fd6eb261e6e2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-25 09:01:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import pandas as pd
 import altair as alt
 from dateutil import parser
 
+time_shift_choice = ["14-16", "16-18", "18-20"]
 
 # Connect to Deta Base with your Project Key
 deta = Deta(st.secrets["deta_key"])
+
+date = st.date_input("Choose a date")
+time_shift = st.multiselect('Chose a time shift',time_shift_choice, time_shift_choice)
 
 # Create a new database
 db = deta.Base("project_fietskliniek")
@@ -15,5 +19,7 @@ db = deta.Base("project_fietskliniek")
 # find if there are available shift in that data
 db_content = db.fetch().items
 df = pd.DataFrame(db_content)
+df_filter = df[(df.date==date) & (df.time_shift==time_shift)]
 
-st.dataframe(df)
+
+st.dataframe(df_filter)
