992e7fb0357da20a01240d70910c47dfb3a691df
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-25 14:04:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ placeholder.text("Hello")
 # Replace the text with a chart:
 placeholder.line_chart({"data": [1, 5, 2, 6]})
 
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
+# # Replace the chart with several elements:
+# with placeholder.container():
+#     st.write("This is one element")
+#     st.write("This is another")
 
 # Clear all those elements:
 # placeholder.empty()
