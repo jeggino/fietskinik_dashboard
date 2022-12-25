70d44add7e96eb9bcf0117b276606dd981665d00
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-25 10:50:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,22 @@ df_filter_data = df[df.date==date]
 if len(df_filter_data)==0:
   st.info('No appointments', icon="ℹ️")
 else:
-  time_shift = st.selectbox('Chose a time shift',time_shift_choice)
-  df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].drop("key",axis=1).sort_values("time_shift").reset_index(drop=True)
-  n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
-  n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
-  n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
   
-  col1, col2, col3 = st.columns(3)
-  col1.metric("14-16", f"{n_1} clients")
-  col2.metric("16-18", f"{n_2} clients")
-  col3.metric("18-20", f"{n_3} clients")
+  left, right = st.columns(3)
+  with left:
+    time_shift = st.selectbox('Chose a time shift',time_shift_choice)
+    
+  with right:    
+    df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].drop("key",axis=1).sort_values("time_shift").reset_index(drop=True)
+    n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
+    n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
+    n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric("14-16", f"{n_1} clients")
+    col2.metric("16-18", f"{n_2} clients")
+    col3.metric("18-20", f"{n_3} clients")
+    
   st.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
 
 
