fca4da337fae53dc119eb4d9ceb7da9094892a73
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-25 14:25:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ if len(df_filter_data)==0:
   st.info('No appointments', icon="ℹ️")
 
 else:
-  placeholder = st.empty()
   col2, col3, col4 = st.columns([1, 1, 1])
   time_shift = st.selectbox('Chose a time shift',time_shift_choice)
   df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
@@ -34,7 +33,8 @@ else:
   col2.metric("14-16", f"{n_1} clients")
   col3.metric("16-18", f"{n_2} clients")
   col4.metric("18-20", f"{n_3} clients")
-  
+  placeholder = st.empty()
+
   placeholder.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
   
   if not st.checkbox('Show table'):
