547fda14b4edfe6fe0384b59a75a4c58d7b528bb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-27 09:15:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ from deta import Deta
 import pandas as pd
 import altair as alt
 
-
-time_shift_choice = ["14-16", "16-18", "18-20"]
-
-# Connect to Deta Base with your Project Key
-deta = Deta(st.secrets["deta_key"])
-date =  str(st.date_input("Choose a date"))
-
 # Show extra buttons for admin users.
 ADMIN_USERS = {
     'person1@email.com',
@@ -18,40 +11,46 @@ ADMIN_USERS = {
     'person3@email.com'
 }
 if st.experimental_user.email in ADMIN_USERS:
-    display_the_extra_admin_buttons()
-display_the_interface_everyone_sees()
+    time_shift_choice = ["14-16", "16-18", "18-20"]
+
+    # Connect to Deta Base with your Project Key
+    deta = Deta(st.secrets["deta_key"])
+    date =  str(st.date_input("Choose a date"))
 
 
-# Create a new database
-db = deta.Base("project_fietskliniek")
-db_content = db.fetch().items
-df = pd.DataFrame(db_content)
-df_filter_data = df[df.date==date]
 
-if len(df_filter_data)==0:
-  st.info('No appointments', icon="ℹ️")
 
-else:
-  col2, col3, col4 = st.columns([1, 1, 1])
-  time_shift_empty = st.empty()
-  time_shift = time_shift_empty.selectbox('Chose a time shift',time_shift_choice)
-  
-  df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
-  
-  n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
-  n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
-  n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
 
-  col2.metric("14-16", f"{n_1} clients")
-  col3.metric("16-18", f"{n_2} clients")
-  col4.metric("18-20", f"{n_3} clients")
-  
-  placeholder = st.empty()
-  placeholder.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
-  
-  if not st.checkbox('Show table'):
-    placeholder.empty()
-    time_shift_empty.empty()
+    # Create a new database
+    db = deta.Base("project_fietskliniek")
+    db_content = db.fetch().items
+    df = pd.DataFrame(db_content)
+    df_filter_data = df[df.date==date]
+
+    if len(df_filter_data)==0:
+      st.info('No appointments', icon="ℹ️")
+
+    else:
+      col2, col3, col4 = st.columns([1, 1, 1])
+      time_shift_empty = st.empty()
+      time_shift = time_shift_empty.selectbox('Chose a time shift',time_shift_choice)
+
+      df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
+
+      n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
+      n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
+      n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
+
+      col2.metric("14-16", f"{n_1} clients")
+      col3.metric("16-18", f"{n_2} clients")
+      col4.metric("18-20", f"{n_3} clients")
+
+      placeholder = st.empty()
+      placeholder.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
+
+      if not st.checkbox('Show table'):
+        placeholder.empty()
+        time_shift_empty.empty()
   
 # Display the contents of the dictionary
 st.write(st.experimental_user)
