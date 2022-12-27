453a944f2cda517fa0142cb7554c4ddc61288b71
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-27 09:42:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,32 +26,33 @@ selected = option_menu(
     menu_title=None,
     options=["Agenda", "Dashboard"],
     icons=["bi-journal-check", "bi-x-octagon-fill"],  # https://icons.getbootstrap.com/
-    orientation="vertical",
+    orientation="horizontal",
 )
 
+if selected == "Agenda":
 
-# Using "with" notation
-if len(df_filter_data)==0:
-  st.info('No appointments', icon="ℹ️")
+    # Using "with" notation
+    if len(df_filter_data)==0:
+      st.info('No appointments', icon="ℹ️")
 
-else:
-  col2, col3, col4 = st.columns([1, 1, 1])
-  time_shift_empty = st.empty()
-  time_shift = time_shift_empty.radio('Chose a time shift',time_shift_choice, horizontal=True)
+    else:
+      col2, col3, col4 = st.columns([1, 1, 1])
+      time_shift_empty = st.empty()
+      time_shift = time_shift_empty.radio('Chose a time shift',time_shift_choice, horizontal=True)
 
-  df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
+      df_filter_time = df_filter_data[df_filter_data.time_shift==time_shift].sort_values("time_shift").reset_index(drop=True)
 
-  n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
-  n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
-  n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
+      n_1 = len(df_filter_data[df_filter_data.time_shift=="14-16"])
+      n_2 = len(df_filter_data[df_filter_data.time_shift=="16-18"])
+      n_3 = len(df_filter_data[df_filter_data.time_shift=="18-20"])
 
-  col2.metric("14-16", f"{n_1} clients")
-  col3.metric("16-18", f"{n_2} clients")
-  col4.metric("18-20", f"{n_3} clients")
+      col2.metric("14-16", f"{n_1} clients")
+      col3.metric("16-18", f"{n_2} clients")
+      col4.metric("18-20", f"{n_3} clients")
 
-  placeholder = st.empty()
-  placeholder.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
+      placeholder = st.empty()
+      placeholder.dataframe(df_filter_time[["name","e_mail","buurt","opmerking","materiaal","werkzaamheedeb"]].T, use_container_width=True)
 
-  if not st.checkbox('Show table'):
-    placeholder.empty()
-    time_shift_empty.empty()
+      if not st.checkbox('Show table'):
+        placeholder.empty()
+        time_shift_empty.empty()
