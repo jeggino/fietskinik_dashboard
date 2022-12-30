7c31e0fe6aed985db6cd08b9b05af5cea86f6ca3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-30 07:32:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,27 +29,16 @@ db_content = db.fetch().items
 df = pd.DataFrame(db_content)
 
 # ---trial---
-# @st.cache(allow_output_mutation=True)
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file)
-#     page_bg_img = '''
-#     <style>
-#     body {
-#     background-image: url("data:image/jpg;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-    
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
-# set_png_as_page_bg("https://live.staticflickr.com/6168/6205061962_b89837be86_b.jpg")
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("_MG_0069_edited.jpg")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # --- NAVIGATION MENU ---
