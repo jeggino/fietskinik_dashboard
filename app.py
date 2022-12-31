fbf2147a2be2f07f35f62f535179f39cf5a99b16
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-31 10:56:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,3 +70,30 @@ if selected == "Agenda":
       if not st.checkbox('Show table'):
         placeholder.empty()
         time_shift_empty.empty()
+        
+if selected == "Dashboard":
+    
+   
+    import numpy as np
+
+    from streamlit_vega_lite import vega_lite_component, altair_component
+
+    hist_data = pd.DataFrame(np.random.normal(42, 10, (200, 1)), columns=["x"])
+
+    @st.cache
+    def altair_histogram():
+        brushed = alt.selection_interval(encodings=["x"], name="brushed")
+
+        return (
+            alt.Chart(hist_data)
+            .mark_bar()
+            .encode(alt.X("x:Q", bin=True), y="count()")
+            .add_selection(brushed)
+        )
+
+    event_dict = altair_component(altair_chart=altair_histogram())
+
+    r = event_dict.get("x")
+    if r:
+        filtered = hist_data[(hist_data.x >= r[0]) & (hist_data.x < r[1])]
+        st.write(filtered)
