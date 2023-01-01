f3eea7b27b4f43b176103a1a7faa688686dd4f06
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-01 11:08:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,11 +109,9 @@ if selected == "Dashboard":
                             tooltip = folium.GeoJsonTooltip(fields = ['image_name'],
                                                             aliases=['Image: '],
                                                             style = ("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
-                                                            sticky = True,
-                                                            name='group on map',
-                                                            localize=True
-                            )
-
+                                                            sticky = True),
+                            name='group on map',
+                            localize=True
                             ).add_to(m)
 
         map_data = st_folium(m, key="fig1")
