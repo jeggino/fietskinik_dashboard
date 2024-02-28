92d03f6ea04f748ee203ed56cd212147099dbc8e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-02-28 10:48:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ from datetime import datetime as dt
 # st.stop()
 # ###
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # ---INSET PASSWORD---
 passwords = ["fietskliniek"]
