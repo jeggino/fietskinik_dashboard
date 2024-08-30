7e360c42cdebf756ee7e0abb7c6a0824fd2cc548
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-08-30 18:08:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -223,10 +223,31 @@ def Agenda():
                         continue
 
 
-def page2():
-    st.write(st.session_state.bar)
+def Statistik():
+            import altair as alt
+            from vega_datasets import data
+            
+            source = data.cars()
+            
+            chart = alt.Chart(source).mark_circle().encode(
+                x='Horsepower',
+                y='Miles_per_Gallon',
+                color='Origin',
+            ).interactive()
+            
+            tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+            
+            with tab1:
+                # Use the Streamlit theme.
+                # This is the default. So you can also omit the theme argument.
+                st.altair_chart(chart, theme="streamlit", use_container_width=True)
+            with tab2:
+                # Use the native Altair theme.
+                st.altair_chart(chart, theme=None, use_container_width=True)
+            
+    
 
 
-pg = st.navigation([st.Page(Agenda), st.Page(page2)])
+pg = st.navigation([st.Page(Agenda), st.Page(Statistik)])
 pg.run()
 
