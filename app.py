2c5a6366beb21c3e76f5bccb14787f7e4f8a2f34
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-08-30 16:58:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ deta = Deta(st.secrets["deta_key"])
 db = deta.Base("db_data")
 db_content = db.fetch().items
 df = pd.DataFrame(db_content)
+drive = deta.Drive("df_pictures")
 
 df['Date'] =  pd.to_datetime(df['Date'], format='%Y-%m-%d')
 df['year'] = df['Date'].dt.year
@@ -181,21 +182,25 @@ elif selected == len_next_week:
         if friday==0:
             st.info('No appointments', icon="ℹ️")
         else:
-            df_filter_next_week_friday = df_filter_next_week[df_filter_next_week["Day"] == "Friday"]
-            st.dataframe(df_filter_next_week_friday)
-
             
-                
+            df_filter_next_week_friday = df_filter_next_week[df_filter_next_week["Day"] == "Friday"]
+                      
             df_show = df_filter_next_week_friday[["Membership","Membership_number","Time shift", "Name", "e_mail", 
                                                   "Phone number", "Neighborhood", "Expertise", "Type of bike",
                                                   "Type of reparation", "Remarks"]].T
             
             columns = []
+            
             for i in range(df_show.shape[1]):
                 columns.append(f"Client {i + 1}")
+                
             df_show.columns = columns
             st.dataframe(df_show, use_container_width=True)
 
             df_pictures_2 = df_filter_next_week_friday["Name_picture"]
             df_pictures_2.index = columns
             df_pictures_2
+            for i, column in df_pictures_2.iterrows():
+                
+                res = drive.get(column["Name_picture"]).read()
+                st.image(res,caption=i)
