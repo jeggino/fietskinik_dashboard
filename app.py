7dc36a677afac0434e851adb38558c2f4999821d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-08-30 18:00:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,21 @@ hide_st_style = """
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+import streamlit as st
+
+def page1():
+    st.write(st.session_state.foo)
+
+def page2():
+    st.write(st.session_state.bar)
+
+# Widgets shared by all the pages
+st.sidebar.selectbox("Foo", ["A", "B", "C"], key="foo")
+st.sidebar.checkbox("Bar", key="bar")
+
+pg = st.navigation([st.Page(page1), st.Page(page2)])
+pg.run()
+
 
 # ---Connect to Deta Base with your Project Key---
 deta = Deta(st.secrets["deta_key"])
