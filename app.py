af04df669f8ac571617f849c2a53cdd79d0d790d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-01 12:09:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -225,6 +225,7 @@ def Agenda():
 def Statistik():
             image = "https://www.thesignmaker.co.nz/wp-content/uploads/2019/04/C16_Work-In-Progress.png"
             st.image(image)
+            st.write("2")
             
     
 
